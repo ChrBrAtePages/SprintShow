7aa091c9bfd50b8e5f469152457c6078a08bae53
HgSlv <h.silva@epages.com>
2017-12-20 16:38:44 +0100

    add slides

--- scripts/jiratasks.js
+++ scripts/jiratasks.js
@@ -1,3 +1,100 @@
 define(function () {
-    return []
+    return [
+    {
+        "id": "EPG-37031",
+        "name": "Paypal Plus: Include order reference",
+        "type": "Story",
+        "summary": "",
+        "priority": "Major",
+        "integrated": "7.19.0",
+        "platform": "ePages6",
+        "links": [
+            
+        ]
+    },
+    {
+        "id": "AD-6852",
+        "name": "Enforce scheduleUpdates while csv-categories-import",
+        "type": "Research",
+        "summary": "<ul><li>hell ojhkjhkjhkhkjhsadf</li></ul>",
+        "priority": "Critical",
+        "integrated": "7.18.0",
+        "platform": "ePages6",
+        "links": [
+            
+        ]
+    },
+    {
+        "id": "AD-7245",
+        "name": "Paypal Plus: Add support for more currencies",
+        "type": "Task",
+        "summary": "",
+        "priority": "Major",
+        "integrated": "7.18.0",
+        "platform": "ePages6",
+        "links": [
+            
+        ]
+    },
+    {
+        "id": "AD-8469",
+        "name": "Coupon: Deleting coupon doesn't delete CouponDiscount in LineItemProduct",
+        "type": "Bug",
+        "summary": "",
+        "priority": "Major",
+        "integrated": "7.18.0",
+        "platform": "ePages6",
+        "links": [
+            
+        ]
+    },
+    {
+        "id": "AD-8510",
+        "name": "DHLversenden: can't handle too long merchant company names",
+        "type": "Bug",
+        "summary": "",
+        "priority": "Blocker",
+        "integrated": "7.18.0",
+        "platform": "ePages6",
+        "links": [
+            
+        ]
+    },
+    {
+        "id": "AD-8512",
+        "name": "Skrill: Remove euro character from api call",
+        "type": "Task",
+        "summary": "",
+        "priority": "Major",
+        "integrated": "7.19.0",
+        "platform": "ePages6",
+        "links": [
+            
+        ]
+    },
+    {
+        "id": "AD-7458",
+        "name": "Ipayment: hint for merchants if order is not paid",
+        "type": "Improvement",
+        "summary": "",
+        "priority": "Major",
+        "integrated": "7.19.0",
+        "platform": "ePages6",
+        "links": [
+            
+        ]
+    },
+    {
+        "id": "AD-8090",
+        "name": "DHLIntraship: Format Name & Postnumber for DHL Intraship",
+        "type": "Improvement",
+        "summary": "",
+        "priority": "Critical",
+        "integrated": "7.18.0",
+        "platform": "ePages6",
+        "links": [
+            
+        ]
+    }
+]
 })
